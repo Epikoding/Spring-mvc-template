9eb7bcefaebbf9e00fbb5206fc917b576488ab09
Don-torder <ldj1@torder.co.kr>
2023-12-05 10:37:11 +0900

    fix: request tuning

--- src/main/java/com/test/template/global/config/QuerydslConfig.java
+++ src/main/java/com/test/template/global/config/QuerydslConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class QuerydslConfiguration {
+public class QuerydslConfig {
 
     @PersistenceContext
     private EntityManager entityManager;
